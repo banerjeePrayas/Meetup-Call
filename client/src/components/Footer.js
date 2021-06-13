094711945d0fa522a52020c2fc34d0114bbd8e75
Prayas Banerjee <rupanreal7@gmail.com>
2021-06-13 20:07:20 +0530

    Ready & Deployed

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+
+
+const Footer = () => {
+    return (
+        <div style={{ textAlign: "center", padding: "2rem", color: '#f1f1f1', fontWeight: 'bolder' }}>
+            <h2>Developed by <a style={{ textDecoration: 'none', color:'orange' }} href='https://www.linkedin.com/in/prayas-banerjee/' target='_blank'>Prayas</a></h2>
+        </div>
+    )
+}
+
+export default Footer
